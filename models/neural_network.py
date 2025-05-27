5f174f6f5591f563d667a62761bc36e77f43400f
WesleySouza13 <souzabusiness13@gmail.com>
2025-05-27 16:07:55 -0300

    atualizando modelos e setando novos neuronios

--- models/neural_network.py
+++ models/neural_network.py
@@ -7,9 +7,13 @@ class neural_network(nn.Module):
     def __init__(self):
         super().__init__()
         self.layer1 = nn.Linear(103, 206)
-        self.layer2 = nn.Linear(206,103)        
-        self.layer3 = nn.Linear(103,50)
-        self.layer4 = nn.Linear(50,1)
+        self.layer2 = nn.Linear(206,412)        
+        self.layer3 = nn.Linear(412,824)
+        self.layer4 = nn.Linear(824,412)
+        self.layer5 = nn.Linear(412, 206)
+        self.layer6 = nn.Linear(206, 103)
+        self.layer7 = nn.Linear(103, 50)
+        self.layer8 = nn.Linear(50,1)
         self.activation = nn.ReLU()
         self.drop = nn.Dropout(0.1)
         
@@ -27,6 +31,22 @@ class neural_network(nn.Module):
         x = self.drop(x)
         
         x = self.layer4(x)
+        x = self.activation(x)
+        x = self.drop(x)
+        
+        x = self.layer5(x)
+        x = self.activation(x)
+        x = self.drop(x)
+        
+        x = self.layer6(x)
+        x = self.activation(x)
+        x = self.drop(x)
+        
+        x = self.layer7(x)
+        x = self.activation(x)
+        x = self.drop(x)
+        
+        x = self.layer8(x)
         
         return x 
         
