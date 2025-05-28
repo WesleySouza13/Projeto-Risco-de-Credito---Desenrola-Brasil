6a9c5862097603898eac10b9971c284b44d434f3
WesleySouza13 <souzabusiness13@gmail.com>
2025-05-28 20:08:53 -0300

    ultimos ajustes

--- models/neural_network.py
+++ models/neural_network.py
@@ -1,53 +1,38 @@
 # %%
 import torch
 from torch import nn
-
 # %%
 class neural_network(nn.Module):
     def __init__(self):
         super().__init__()
-        self.layer1 = nn.Linear(103, 206)
-        self.layer2 = nn.Linear(206,412)        
-        self.layer3 = nn.Linear(412,824)
-        self.layer4 = nn.Linear(824,412)
-        self.layer5 = nn.Linear(412, 206)
-        self.layer6 = nn.Linear(206, 103)
-        self.layer7 = nn.Linear(103, 50)
-        self.layer8 = nn.Linear(50,1)
-        self.activation = nn.ReLU()
-        self.drop = nn.Dropout(0.1)
+        self.layer1 = nn.Linear(103, 256)
+        self.bn1 = nn.BatchNorm1d(256)
+        self.layer2 = nn.Linear(256,128)  
+        self.bn2 = nn.BatchNorm1d(128)      
+        self.layer3 = nn.Linear(128,64)
+        self.bn3 = nn.BatchNorm1d(64)
+        self.layer4 = nn.Linear(64,1)
+        self.activation = nn.LeakyReLU()
+        self.drop = nn.Dropout(0.2)
         
     def forward(self,x):
         x = self.layer1(x)
+        x = self.bn1(x)
         x = self.activation(x)
         x = self.drop(x)
 
         x = self.layer2(x)
+        x = self.bn2(x)
         x = self.activation(x)
         x = self.drop(x)
 
         x = self.layer3(x)
+        x = self.bn3(x)
         x = self.activation(x)
         x = self.drop(x)
         
         x = self.layer4(x)
-        x = self.activation(x)
-        x = self.drop(x)
-        
-        x = self.layer5(x)
-        x = self.activation(x)
-        x = self.drop(x)
-        
-        x = self.layer6(x)
-        x = self.activation(x)
-        x = self.drop(x)
-        
-        x = self.layer7(x)
-        x = self.activation(x)
-        x = self.drop(x)
-        
-        x = self.layer8(x)
-        
+
         return x 
         
 
